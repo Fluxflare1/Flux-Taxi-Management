f6a067359bedbcbb5e022684d409b26e7665945a
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 13:02:45 +0100

    Create LoginForm.js

---
+++ Flux Taxi Management System/frontend/src/components/LoginForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/login/', { username, password })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log('Error logging in', error);
+            });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default LoginForm;

74a977592273accc725e632d9922c8d7da7d1f53
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 16:27:35 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/subscription_service/serializers.py
+++ Flux Taxi Management System/backend/subscription_service/serializers.py
@@ -1,1 +1,14 @@
+# subscription_service/serializers.py
 
+from rest_framework import serializers
+from .models import SubscriptionPlan, UserSubscription
+
+class SubscriptionPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubscriptionPlan
+        fields = '__all__'
+
+class UserSubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserSubscription
+        fields = '__all__'

b40cd43e33ad023f178f7ee8e4932c709bb48fd7
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:04:48 +0100

    Update urls.py

--- Flux Taxi Management System/backend/airport service/urls.py
+++ Flux Taxi Management System/backend/airport service/urls.py
@@ -1,1 +1,12 @@
+# airport_service/urls.py
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import AirportTransferViewSet
+
+router = DefaultRouter()
+router.register(r'airport-transfers', AirportTransferViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

ffb667790a43471f1887c50b0a1e194015a24443
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:52:04 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/carpooling/serializers.py
+++ Flux Taxi Management System/backend/carpooling/serializers.py
@@ -1,1 +1,14 @@
+# carpooling/serializers.py
 
+from rest_framework import serializers
+from .models import CarpoolRide, CarpoolBooking
+
+class CarpoolRideSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarpoolRide
+        fields = '__all__'
+
+class CarpoolBookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarpoolBooking
+        fields = '__all__'

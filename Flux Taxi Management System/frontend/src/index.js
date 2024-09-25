b94b30bae7f195e8cc41fbadde0bc57a3bf62cb1
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 12:45:13 +0100

    Create index.js

---
+++ Flux Taxi Management System/frontend/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Auth from './components/Auth';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Auth />
+  </React.StrictMode>,
+  document.getElementById('root')
+);

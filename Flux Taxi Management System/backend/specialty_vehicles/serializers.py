2335035c020b48515c8571c036cfc487405233dc
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:35:45 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/specialty_vehicles/serializers.py
+++ Flux Taxi Management System/backend/specialty_vehicles/serializers.py
@@ -1,1 +1,14 @@
+# specialty_vehicles/serializers.py
 
+from rest_framework import serializers
+from .models import SpecialtyVehicle, VehicleBooking
+
+class SpecialtyVehicleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SpecialtyVehicle
+        fields = '__all__'
+
+class VehicleBookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VehicleBooking
+        fields = '__all__'

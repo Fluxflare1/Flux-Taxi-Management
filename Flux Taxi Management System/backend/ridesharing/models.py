daf34724301ba2c9ee9fbcbff75098f8684ed294
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 13:26:40 +0100

    Create models.py

---
+++ Flux Taxi Management System/backend/ridesharing/models.py
@@ -0,0 +1,13 @@
+from django.contrib.gis.db import models
+from django.contrib.auth.models import User
+
+class RideSharing(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_rides')
+    pickup_location = models.PointField()
+    destination_location = models.PointField()
+    max_participants = models.IntegerField(default=4)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+class RideSharingParticipant(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ride_sharing = models.ForeignKey(RideSharing, on_delete=models.CASCADE, related_name='participants')

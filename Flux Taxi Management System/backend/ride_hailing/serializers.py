82e01cf7a312a2f134884d0192db0c4e1c7f5d32
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 14:02:26 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/ride_hailing/serializers.py
+++ Flux Taxi Management System/backend/ride_hailing/serializers.py
@@ -1,1 +1,9 @@
+# ride_hailing/serializers.py
 
+from rest_framework import serializers
+from .models import Ride
+
+class RideSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ride
+        fields = '__all__'

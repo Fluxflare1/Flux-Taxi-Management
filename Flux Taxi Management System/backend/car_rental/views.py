038c6446a6cb87c9942141d226ff9ad6f2eaee51
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 14:30:42 +0100

    Update views.py

--- Flux Taxi Management System/backend/car_rental/views.py
+++ Flux Taxi Management System/backend/car_rental/views.py
@@ -1,1 +1,13 @@
+# car_rental/views.py
 
+from rest_framework import viewsets
+from .models import Car, Rental
+from .serializers import CarSerializer, RentalSerializer
+
+class CarViewSet(viewsets.ModelViewSet):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+class RentalViewSet(viewsets.ModelViewSet):
+    queryset = Rental.objects.all()
+    serializer_class = RentalSerializer

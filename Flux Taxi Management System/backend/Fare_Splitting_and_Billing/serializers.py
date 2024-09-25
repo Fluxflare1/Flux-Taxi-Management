eb577126e0e0383e49b31ccee70eef4dd1358b12
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 16:51:03 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/Fare_Splitting_and_Billing/serializers.py
+++ Flux Taxi Management System/backend/Fare_Splitting_and_Billing/serializers.py
@@ -1,1 +1,9 @@
+# fare_splitting_billing/serializers.py
 
+from rest_framework import serializers
+from .models import FareSplit
+
+class FareSplitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FareSplit
+        fields = '__all__'

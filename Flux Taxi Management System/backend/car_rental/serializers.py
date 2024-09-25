c7f15fd5eb8db8b8a84baeb319385303dd01c2ca
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 14:31:40 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/car_rental/serializers.py
+++ Flux Taxi Management System/backend/car_rental/serializers.py
@@ -1,1 +1,14 @@
+# car_rental/serializers.py
 
+from rest_framework import serializers
+from .models import Car, Rental
+
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+class RentalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rental
+        fields = '__all__'

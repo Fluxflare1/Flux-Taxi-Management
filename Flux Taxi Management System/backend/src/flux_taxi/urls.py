3d8f4e7f25eb91464d0a7e5d3f92150fe50b9c59
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 14:23:15 +0100

    Update urls.py

--- Flux Taxi Management System/backend/src/flux_taxi/urls.py
+++ Flux Taxi Management System/backend/src/flux_taxi/urls.py
@@ -1,3 +1,12 @@
+# project/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('ride-hailing/', include('ride_hailing.urls')),
+]
 from django.contrib import admin
 from django.urls import path, include
 

d9f498c45b374b7dadf3fe7781f6b13413dd74f0
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 16:29:09 +0100

    Update urls.py

--- Flux Taxi Management System/backend/subscription_service/urls.py
+++ Flux Taxi Management System/backend/subscription_service/urls.py
@@ -1,1 +1,13 @@
+# subscription_service/urls.py
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import SubscriptionPlanViewSet, UserSubscriptionViewSet
+
+router = DefaultRouter()
+router.register(r'plans', SubscriptionPlanViewSet)
+router.register(r'user-subscriptions', UserSubscriptionViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

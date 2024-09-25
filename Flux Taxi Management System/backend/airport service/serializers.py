b73d81fea83408d71870f4507d85225ecae303f0
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:04:08 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/airport service/serializers.py
+++ Flux Taxi Management System/backend/airport service/serializers.py
@@ -1,1 +1,9 @@
+# airport_service/serializers.py
 
+from rest_framework import serializers
+from .models import AirportTransfer
+
+class AirportTransferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AirportTransfer
+        fields = '__all__'

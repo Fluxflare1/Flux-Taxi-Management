f5683358d077b5d45c96a466bdcb654ea6083514
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:16:04 +0100

    Update views.py

--- Flux Taxi Management System/backend/fixed_route_taxi/views.py
+++ Flux Taxi Management System/backend/fixed_route_taxi/views.py
@@ -1,1 +1,13 @@
+# fixed_route_taxi/views.py
 
+from rest_framework import viewsets
+from .models import FixedRoute, FixedRouteBooking
+from .serializers import FixedRouteSerializer, FixedRouteBookingSerializer
+
+class FixedRouteViewSet(viewsets.ModelViewSet):
+    queryset = FixedRoute.objects.all()
+    serializer_class = FixedRouteSerializer
+
+class FixedRouteBookingViewSet(viewsets.ModelViewSet):
+    queryset = FixedRouteBooking.objects.all()
+    serializer_class = FixedRouteBookingSerializer

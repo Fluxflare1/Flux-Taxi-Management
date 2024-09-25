d5345ed95827f6d107ab0343a961cfa99ba1223a
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:24:16 +0100

    Update views.py

--- Flux Taxi Management System/backend/driver_aggregator/views.py
+++ Flux Taxi Management System/backend/driver_aggregator/views.py
@@ -1,1 +1,13 @@
+# driver_aggregator/views.py
 
+from rest_framework import viewsets
+from .models import Driver, RideRequest
+from .serializers import DriverSerializer, RideRequestSerializer
+
+class DriverViewSet(viewsets.ModelViewSet):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+
+class RideRequestViewSet(viewsets.ModelViewSet):
+    queryset = RideRequest.objects.all()
+    serializer_class = RideRequestSerializer

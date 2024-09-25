567cf9d72f31de526ef54ec9a71ba5baf09baa6e
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 16:00:55 +0100

    Update views.py

--- Flux Taxi Management System/backend/shuttle_service/views.py
+++ Flux Taxi Management System/backend/shuttle_service/views.py
@@ -1,1 +1,13 @@
+# shuttle_service/views.py
 
+from rest_framework import viewsets
+from .models import ShuttleRoute, ShuttleBooking
+from .serializers import ShuttleRouteSerializer, ShuttleBookingSerializer
+
+class ShuttleRouteViewSet(viewsets.ModelViewSet):
+    queryset = ShuttleRoute.objects.all()
+    serializer_class = ShuttleRouteSerializer
+
+class ShuttleBookingViewSet(viewsets.ModelViewSet):
+    queryset = ShuttleBooking.objects.all()
+    serializer_class = ShuttleBookingSerializer

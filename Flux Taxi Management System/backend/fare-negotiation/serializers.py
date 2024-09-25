94734371ae8b4de0c9717b606d4769a0c32d2f0e
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 16:33:04 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/fare-negotiation/serializers.py
+++ Flux Taxi Management System/backend/fare-negotiation/serializers.py
@@ -1,1 +1,9 @@
+# fare_negotiation/serializers.py
 
+from rest_framework import serializers
+from .models import FareNegotiation
+
+class FareNegotiationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FareNegotiation
+        fields = '__all__'

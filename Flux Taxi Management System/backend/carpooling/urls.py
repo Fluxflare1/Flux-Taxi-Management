6f954e88e72054dbc518f0ba568c8bd684222c2a
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:52:37 +0100

    Update urls.py

--- Flux Taxi Management System/backend/carpooling/urls.py
+++ Flux Taxi Management System/backend/carpooling/urls.py
@@ -1,1 +1,13 @@
+# carpooling/urls.py
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CarpoolRideViewSet, CarpoolBookingViewSet
+
+router = DefaultRouter()
+router.register(r'rides', CarpoolRideViewSet)
+router.register(r'bookings', CarpoolBookingViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

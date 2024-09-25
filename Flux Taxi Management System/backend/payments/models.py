6b2272735e1709f90a19accc5ae16c62e92b099e
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 13:20:03 +0100

    Create models.py

---
+++ Flux Taxi Management System/backend/payments/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    reference = models.CharField(max_length=200, unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True)

3a64ee6f71810fc240fdc7a6be92e5bc505a5410
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 16:33:43 +0100

    Update urls.py

--- Flux Taxi Management System/backend/fare-negotiation/urls.py
+++ Flux Taxi Management System/backend/fare-negotiation/urls.py
@@ -1,1 +1,12 @@
+# fare_negotiation/urls.py
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import FareNegotiationViewSet
+
+router = DefaultRouter()
+router.register(r'negotiations', FareNegotiationViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

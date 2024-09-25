0eeee09f97fd46068536a2450f9ce6e352e60eb5
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:35:12 +0100

    Update views.py

--- Flux Taxi Management System/backend/specialty_vehicles/views.py
+++ Flux Taxi Management System/backend/specialty_vehicles/views.py
@@ -1,1 +1,13 @@
+# specialty_vehicles/views.py
 
+from rest_framework import viewsets
+from .models import SpecialtyVehicle, VehicleBooking
+from .serializers import SpecialtyVehicleSerializer, VehicleBookingSerializer
+
+class SpecialtyVehicleViewSet(viewsets.ModelViewSet):
+    queryset = SpecialtyVehicle.objects.all()
+    serializer_class = SpecialtyVehicleSerializer
+
+class VehicleBookingViewSet(viewsets.ModelViewSet):
+    queryset = VehicleBooking.objects.all()
+    serializer_class = VehicleBookingSerializer

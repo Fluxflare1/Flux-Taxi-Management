fec77281db11a6ed7edbaeefa8f6910875eb1ea9
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 14:41:07 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/outstation_service/serializers.py
+++ Flux Taxi Management System/backend/outstation_service/serializers.py
@@ -1,1 +1,9 @@
+# outstation_service/serializers.py
 
+from rest_framework import serializers
+from .models import OutstationTrip
+
+class OutstationTripSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OutstationTrip
+        fields = '__all__'

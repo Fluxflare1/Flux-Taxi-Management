77965c4c03b8d4f0a72425ff09e82b999f91627a
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 16:28:23 +0100

    Update views.py

--- Flux Taxi Management System/backend/subscription_service/views.py
+++ Flux Taxi Management System/backend/subscription_service/views.py
@@ -1,1 +1,26 @@
+# subscription_service/views.py
 
+from rest_framework import viewsets
+from .models import SubscriptionPlan, UserSubscription
+from .serializers import SubscriptionPlanSerializer, UserSubscriptionSerializer
+from rest_framework.response import Response
+from django.utils import timezone
+
+class SubscriptionPlanViewSet(viewsets.ModelViewSet):
+    queryset = SubscriptionPlan.objects.all()
+    serializer_class = SubscriptionPlanSerializer
+
+class UserSubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = UserSubscription.objects.all()
+    serializer_class = UserSubscriptionSerializer
+
+    def create(self, request, *args, **kwargs):
+        plan_id = request.data.get('plan_id')
+        plan = SubscriptionPlan.objects.get(id=plan_id)
+        end_date = timezone.now() + timezone.timedelta(days=30)  # Assuming monthly plan
+        user_subscription = UserSubscription.objects.create(
+            user=request.user,
+            plan=plan,
+            end_date=end_date
+        )
+        return Response({'message': 'Subscription created successfully', 'subscription_id': user_subscription.id})

9b6ae9f32b45824f35a09694ab0b62d62d8cdc13
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:51:15 +0100

    Update views.py

--- Flux Taxi Management System/backend/carpooling/views.py
+++ Flux Taxi Management System/backend/carpooling/views.py
@@ -1,1 +1,13 @@
+# carpooling/views.py
 
+from rest_framework import viewsets
+from .models import CarpoolRide, CarpoolBooking
+from .serializers import CarpoolRideSerializer, CarpoolBookingSerializer
+
+class CarpoolRideViewSet(viewsets.ModelViewSet):
+    queryset = CarpoolRide.objects.all()
+    serializer_class = CarpoolRideSerializer
+
+class CarpoolBookingViewSet(viewsets.ModelViewSet):
+    queryset = CarpoolBooking.objects.all()
+    serializer_class = CarpoolBookingSerializer

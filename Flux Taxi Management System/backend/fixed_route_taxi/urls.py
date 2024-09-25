d7fcb3e2a9ab8586c1ddb2722ed67a5a4ab7e18b
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:17:34 +0100

    Update urls.py

--- Flux Taxi Management System/backend/fixed_route_taxi/urls.py
+++ Flux Taxi Management System/backend/fixed_route_taxi/urls.py
@@ -1,1 +1,13 @@
+# fixed_route_taxi/urls.py
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import FixedRouteViewSet, FixedRouteBookingViewSet
+
+router = DefaultRouter()
+router.register(r'fixed-routes', FixedRouteViewSet)
+router.register(r'fixed-route-bookings', FixedRouteBookingViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

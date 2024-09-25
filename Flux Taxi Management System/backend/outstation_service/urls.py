091cd3b1755df1e16907c399e749d459ae36a292
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 14:41:55 +0100

    Update urls.py

--- Flux Taxi Management System/backend/outstation_service/urls.py
+++ Flux Taxi Management System/backend/outstation_service/urls.py
@@ -1,1 +1,12 @@
+# outstation_service/urls.py
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import OutstationTripViewSet
+
+router = DefaultRouter()
+router.register(r'outstation-trips', OutstationTripViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

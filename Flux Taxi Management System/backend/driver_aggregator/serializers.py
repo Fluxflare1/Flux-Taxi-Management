4b501b44256a14dee6caeec37db18470aa730a8d
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:26:24 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/driver_aggregator/serializers.py
+++ Flux Taxi Management System/backend/driver_aggregator/serializers.py
@@ -1,1 +1,14 @@
+# driver_aggregator/serializers.py
 
+from rest_framework import serializers
+from .models import Driver, RideRequest
+
+class DriverSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Driver
+        fields = '__all__'
+
+class RideRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RideRequest
+        fields = '__all__'

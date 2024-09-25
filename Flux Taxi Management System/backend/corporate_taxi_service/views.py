8d1f7243fafba768d1e9b48c8a6d7eaf9539879d
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 14:53:42 +0100

    Update views.py

--- Flux Taxi Management System/backend/corporate_taxi_service/views.py
+++ Flux Taxi Management System/backend/corporate_taxi_service/views.py
@@ -1,1 +1,13 @@
+# corporate_taxi_service/views.py
 
+from rest_framework import viewsets
+from .models import CorporateAccount, CorporateTrip
+from .serializers import CorporateAccountSerializer, CorporateTripSerializer
+
+class CorporateAccountViewSet(viewsets.ModelViewSet):
+    queryset = CorporateAccount.objects.all()
+    serializer_class = CorporateAccountSerializer
+
+class CorporateTripViewSet(viewsets.ModelViewSet):
+    queryset = CorporateTrip.objects.all()
+    serializer_class = CorporateTripSerializer

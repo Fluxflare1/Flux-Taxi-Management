61f3f56e0d507cae59f4f482420b578045eeb6f7
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 19:32:26 +0100

    Update urls.py

--- Flux Taxi Management System/backend/src/flux_taxi/ride_hailing/urls.py
+++ Flux Taxi Management System/backend/src/flux_taxi/ride_hailing/urls.py
@@ -1,1 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import DriverViewSet, RideRequestViewSet, RideHistoryViewSet
 
+router = DefaultRouter()
+router.register('drivers', DriverViewSet)
+router.register('rides', RideRequestViewSet)
+router.register('history', RideHistoryViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

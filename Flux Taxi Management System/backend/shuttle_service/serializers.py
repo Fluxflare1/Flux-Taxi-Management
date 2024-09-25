cc8bb3c08f5f0af56740839b561816765deed72c
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 16:01:33 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/shuttle_service/serializers.py
+++ Flux Taxi Management System/backend/shuttle_service/serializers.py
@@ -1,1 +1,14 @@
+# shuttle_service/serializers.py
 
+from rest_framework import serializers
+from .models import ShuttleRoute, ShuttleBooking
+
+class ShuttleRouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShuttleRoute
+        fields = '__all__'
+
+class ShuttleBookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShuttleBooking
+        fields = '__all__'

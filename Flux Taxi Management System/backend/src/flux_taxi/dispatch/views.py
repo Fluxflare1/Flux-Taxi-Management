d4e201eab5848411207a7cc5f815215d49fdaecc
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 18:57:28 +0100

    Update views.py

--- Flux Taxi Management System/backend/src/flux_taxi/dispatch/views.py
+++ Flux Taxi Management System/backend/src/flux_taxi/dispatch/views.py
@@ -1,1 +1,18 @@
+from rest_framework import viewsets
+from .models import Dispatcher, RideRequest
+from .serializers import DispatcherSerializer, RideRequestSerializer
+from rest_framework.permissions import IsAuthenticated
 
+class DispatcherViewSet(viewsets.ModelViewSet):
+    queryset = Dispatcher.objects.all()
+    serializer_class = DispatcherSerializer
+    permission_classes = [IsAuthenticated]
+
+class RideRequestViewSet(viewsets.ModelViewSet):
+    queryset = RideRequest.objects.all()
+    serializer_class = RideRequestSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically assign a dispatcher when creating a ride request
+        serializer.save(dispatcher=self.request.user.dispatcher)

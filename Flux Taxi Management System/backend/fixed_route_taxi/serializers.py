208d70319ba6992a236e0b9c0543b57c8225c44e
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:16:42 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/fixed_route_taxi/serializers.py
+++ Flux Taxi Management System/backend/fixed_route_taxi/serializers.py
@@ -1,1 +1,14 @@
+# fixed_route_taxi/serializers.py
 
+from rest_framework import serializers
+from .models import FixedRoute, FixedRouteBooking
+
+class FixedRouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FixedRoute
+        fields = '__all__'
+
+class FixedRouteBookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FixedRouteBooking
+        fields = '__all__'

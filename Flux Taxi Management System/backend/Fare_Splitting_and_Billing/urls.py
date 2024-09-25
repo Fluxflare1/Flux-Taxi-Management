a1481ec371b7d94c0ae2f708dcdc874cfcce1256
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 16:51:50 +0100

    Update urls.py

--- Flux Taxi Management System/backend/Fare_Splitting_and_Billing/urls.py
+++ Flux Taxi Management System/backend/Fare_Splitting_and_Billing/urls.py
@@ -1,1 +1,12 @@
+# fare_splitting_billing/urls.py
 
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import FareSplitViewSet
+
+router = DefaultRouter()
+router.register(r'splits', FareSplitViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]

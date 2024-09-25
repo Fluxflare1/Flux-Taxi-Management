25fcda8a07445bc9632581cdafab61ead4271e2d
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 19:31:38 +0100

    Update views.py

--- Flux Taxi Management System/backend/src/flux_taxi/ride_hailing/views.py
+++ Flux Taxi Management System/backend/src/flux_taxi/ride_hailing/views.py
@@ -1,1 +1,19 @@
+from rest_framework import viewsets
+from .models import Driver, RideRequest, RideHistory
+from .serializers import DriverSerializer, RideRequestSerializer, RideHistorySerializer
+from rest_framework.permissions import IsAuthenticated
 
+class DriverViewSet(viewsets.ModelViewSet):
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+    permission_classes = [IsAuthenticated]
+
+class RideRequestViewSet(viewsets.ModelViewSet):
+    queryset = RideRequest.objects.all()
+    serializer_class = RideRequestSerializer
+    permission_classes = [IsAuthenticated]
+
+class RideHistoryViewSet(viewsets.ModelViewSet):
+    queryset = RideHistory.objects.all()
+    serializer_class = RideHistorySerializer
+    permission_classes = [IsAuthenticated]

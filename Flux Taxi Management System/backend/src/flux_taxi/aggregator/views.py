706fab013f3cedd5053906d99ccb0160864e3262
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 18:39:29 +0100

    Update views.py

--- Flux Taxi Management System/backend/src/flux_taxi/aggregator/views.py
+++ Flux Taxi Management System/backend/src/flux_taxi/aggregator/views.py
@@ -1,1 +1,18 @@
+from rest_framework import viewsets
+from .models import FleetOwner, FleetDriver
+from .serializers import FleetOwnerSerializer, FleetDriverSerializer
+from rest_framework.permissions import IsAuthenticated
 
+class FleetOwnerViewSet(viewsets.ModelViewSet):
+    queryset = FleetOwner.objects.all()
+    serializer_class = FleetOwnerSerializer
+    permission_classes = [IsAuthenticated]
+
+class FleetDriverViewSet(viewsets.ModelViewSet):
+    queryset = FleetDriver.objects.all()
+    serializer_class = FleetDriverSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # Return drivers for the authenticated fleet owner
+        return FleetDriver.objects.filter(fleet_owner__owner=self.request.user)

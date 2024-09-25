83d3da112879c6edcf5abee79e2e66aa522c6cea
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 14:54:16 +0100

    Update serializers.py

--- Flux Taxi Management System/backend/corporate_taxi_service/serializers.py
+++ Flux Taxi Management System/backend/corporate_taxi_service/serializers.py
@@ -1,1 +1,14 @@
+# corporate_taxi_service/serializers.py
 
+from rest_framework import serializers
+from .models import CorporateAccount, CorporateTrip
+
+class CorporateAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CorporateAccount
+        fields = '__all__'
+
+class CorporateTripSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CorporateTrip
+        fields = '__all__'

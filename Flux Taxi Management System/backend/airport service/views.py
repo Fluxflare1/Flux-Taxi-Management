878cda1245b410f55d84bc18341e35aed81f08a0
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 15:03:13 +0100

    Update views.py

--- Flux Taxi Management System/backend/airport service/views.py
+++ Flux Taxi Management System/backend/airport service/views.py
@@ -1,1 +1,9 @@
+# airport_service/views.py
 
+from rest_framework import viewsets
+from .models import AirportTransfer
+from .serializers import AirportTransferSerializer
+
+class AirportTransferViewSet(viewsets.ModelViewSet):
+    queryset = AirportTransfer.objects.all()
+    serializer_class = AirportTransferSerializer

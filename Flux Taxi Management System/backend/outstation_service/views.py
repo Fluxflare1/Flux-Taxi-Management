3b89707ec848b67cacc777bb4d0da1b0377e47ce
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-25 14:40:25 +0100

    Update views.py

--- Flux Taxi Management System/backend/outstation_service/views.py
+++ Flux Taxi Management System/backend/outstation_service/views.py
@@ -1,1 +1,9 @@
+# outstation_service/views.py
 
+from rest_framework import viewsets
+from .models import OutstationTrip
+from .serializers import OutstationTripSerializer
+
+class OutstationTripViewSet(viewsets.ModelViewSet):
+    queryset = OutstationTrip.objects.all()
+    serializer_class = OutstationTripSerializer

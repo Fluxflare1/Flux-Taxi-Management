c9ec514d5193c51130e3f213fe34a8125ffe133f
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-26 09:09:58 +0100

    Create settings.py

---
+++ Flux Taxi Management System/backend/settings.py
@@ -0,0 +1,15 @@
+# flux_taxi/settings.py
+INSTALLED_APPS = [
+    ...,
+    'debug_toolbar',
+]
+
+MIDDLEWARE = [
+    ...,
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+]

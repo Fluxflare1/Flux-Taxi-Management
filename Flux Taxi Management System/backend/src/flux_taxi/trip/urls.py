09d4d58d521d1b2f05088b0998c6028534c47d91
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-26 08:02:21 +0100

    Update urls.py

--- Flux Taxi Management System/backend/src/flux_taxi/trip/urls.py
+++ Flux Taxi Management System/backend/src/flux_taxi/trip/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+urlpatterns += [
+    path('create/', views.create_trip, name='create_trip'),
+]
+from django.urls import path
+from . import views
+
 urlpatterns = [
     path('assign/', views.assign_trip, name='assign_trip'),
 ]

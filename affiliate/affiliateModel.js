4a6dfbe6e08be56d3fb705d4b92e660d4b062466
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-26 17:36:22 +0100

    Update affiliateModel.js

--- affiliate/affiliateModel.js
+++ affiliate/affiliateModel.js
@@ -1,1 +1,11 @@
+// affiliate/affiliateModel.js
+const mongoose = require('mongoose');
 
+const affiliateSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    referralCode: { type: String, unique: true },
+    earnings: { type: Number, default: 0 },
+    referrals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+});
+
+module.exports = mongoose.model('Affiliate', affiliateSchema);

1019c6c65c46d3957376be30b3e9ef7ded7ccd79
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-26 17:30:58 +0100

    Delete affiliate directory

--- affiliate/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const affiliateRoutes = require('./routes/affiliateRoutes');
-
-const router = express.Router();
-
-router.use('/affiliate', affiliateRoutes);
-
-module.exports = router;

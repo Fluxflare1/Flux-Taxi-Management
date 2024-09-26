cb98af9eb1e54c7d1f97ca3e5c89fce1a86a0bc4
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-26 17:19:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const affiliateModule = require('./affiliate');
+
+const app = express();
+app.use(express.json());
+
+// Other middleware and routes...
+
+app.use('/api', affiliateModule);
+
+// Connect to MongoDB and start server...
 

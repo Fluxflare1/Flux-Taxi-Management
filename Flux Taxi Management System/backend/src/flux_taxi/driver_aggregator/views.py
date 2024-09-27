10711daf7d7ad439edcd7c2197f8d0ad85cc919b
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-27 21:11:20 +0100

    Update views.py

--- Flux Taxi Management System/backend/src/flux_taxi/driver_aggregator/views.py
+++ Flux Taxi Management System/backend/src/flux_taxi/driver_aggregator/views.py
@@ -1,3 +1,14 @@
+from django.shortcuts import render
+from .fare_calculator import DriverAggregatorFareCalculator
+
+def estimate_fare(request):
+    estimated_fare = None
+    if request.method == 'POST':
+        distance_km = float(request.POST.get('distance_km'))
+        fare_calculator = DriverAggregatorFareCalculator(distance_km)
+        estimated_fare = fare_calculator.calculate_fare()
+
+    return render(request, 'driver_aggregator/estimate_fare.html', {'estimated_fare': estimated_fare})
 # flux_taxi/driver_aggregator/views.py
 
 from django.shortcuts import render, redirect

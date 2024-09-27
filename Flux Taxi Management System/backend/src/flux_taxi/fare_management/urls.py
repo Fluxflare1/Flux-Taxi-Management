240eb8fc19877e86ccfa45a58e5736f297bf8bc5
Fluxflare1 <fluxflareltd@gmail.com>
2024-09-27 20:10:12 +0100

    Create urls.py

---
+++ Flux Taxi Management System/backend/src/flux_taxi/fare_management/urls.py
@@ -0,0 +1,8 @@
+# flux_taxi/fare_management/urls.py
+from django.urls import path
+from .views import estimate_fare, ride_fare
+
+urlpatterns = [
+    path('estimate/', estimate_fare, name='estimate_fare'),
+    path('ride/<int:ride_request_id>/fare/', ride_fare, name='ride_fare'),
+]
